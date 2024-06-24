7f7abf33c4a7d16b8bd4252287784fa27b456bc0
DonOmalVindula <omalvindula@live.com>
2024-06-24 23:20:47 +0530

    Change to env files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from "@asgardeo/auth-react";
-import config from "./config.json";
+
+const config = {
+  signInRedirectURL: import.meta.env.VITE_SIGN_IN_REDIRECT_URL,
+  signOutRedirectURL: import.meta.env.VITE_SIGN_OUT_REDIRECT_URL,
+  clientID: import.meta.env.VITE_CLIENT_ID,
+  baseUrl: import.meta.env.VITE_BASE_URL,
+  scope: import.meta.env.VITE_SCOPE.split(','),
+  resourceServerURLs: import.meta.env.VITE_RESOURCE_SERVER_URLS.split(',')
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
